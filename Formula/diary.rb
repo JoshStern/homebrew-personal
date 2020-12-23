a5b229d4e50bca7601cbecebf7582c7d0938c7a4
Josh Stern <josh.stern@compass.com>
2020-12-23 14:31:08 -0500

    Update diary formula to include latest release

--- Formula/diary.rb
+++ Formula/diary.rb
@@ -1,17 +1,16 @@
 class Diary < Formula
   desc "Simple CLI diary"
   homepage "https://github.com/JoshStern/diary"
-  url "https://github.com/JoshStern/diary/archive/v0.1.0.tar.gz"
-  sha256 "a1844157c34ad7bd22df4cb99aecf80337ba5a6d5e582d689142270cda1219f1"
+  url "https://github.com/JoshStern/diary/archive/v0.2.0.tar.gz"
+  sha256 "5d1fdcc6af1a3062ce47fbca2c981f51c80e1b1c786b5f533717535ba62d20b3"
   license "GPL-3.0-or-later"
 
-  depends_on "git"
   def install
     bin.install "bin/diary.sh" => "diary"
   end
 
   test do
-    system "#{bin}/diary init #{testpath}/test_diary"
+    system "#{bin}/diary", init, "#{testpath}/test_diary"
     assert_predicate "#{testpath}/test_diary", :exist?
   end
 end
